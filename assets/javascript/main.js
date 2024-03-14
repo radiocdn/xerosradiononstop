a7bc28f816786154106072bf0e111f673c015c02
flyindemon <104937168+flyindemon@users.noreply.github.com>
2024-03-14 16:33:48 +0100

    Update main.js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,19 +1,19 @@
-// Bescherming tegen afbeeldingsdiefstal | XerosRadio
+// Bescherming tegen afbeeldingsdiefstal | XerosRadioNonstop
 document.addEventListener('DOMContentLoaded', function () {
     // Selecteer alle afbeeldingen op de pagina
     const afbeeldingen = document.querySelectorAll('img');
   
-    // Voorkom het standaard contextmenu en toon een aangepopt bericht | XerosRadio
+    // Voorkom het standaard contextmenu en toon een aangepopt bericht | XerosRadioNonstop
     function toonAangepoptBericht(event) {
       event.preventDefault();
       alert('U mag geen afbeeldingen van onze website stelen zonder toestemming!');
     }
   
-    // Voeg een eventlistener toe aan elke afbeelding om met rechts klikken om te gaan | XerosRadio
+    // Voeg een eventlistener toe aan elke afbeelding om met rechts klikken om te gaan | XerosRadioNonstop
     afbeeldingen.forEach((img) => {
       img.addEventListener('contextmenu', toonAangepoptBericht);
       
-      // Voorkom slepen en kopiëren van afbeeldingen | XerosRadio
+      // Voorkom slepen en kopiëren van afbeeldingen | XerosRadioNonstop
       img.setAttribute('draggable', 'false');
       img.addEventListener('dragstart', function (e) {
         e.preventDefault();
